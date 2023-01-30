7ada9692976495a1021b56bc3ac83c5d3ea10dde
RowanElsworth <rowanelsworth@gmail.com>
2023-01-30 23:58:13 +0000

    Done the base of the landing page

--- mainapp/src/StyledComponents.js
+++ mainapp/src/StyledComponents.js
@@ -1,43 +1,117 @@
-import styled from "styled-components";
 
-export const HeadSection = styled('div')`
-    top:0;
-    left:0;
-    position:fixed;
-    min-width:100vw;
-    max-width:100%;
-    height: 80px;
-    box-shadow: 0px 0px 10px 5px #777777;
+import styled from "styled-components";
+import "./variables.css"
+
+import hero from "./hero-image.png"
+
+// Section boilerplate
+export const Section = styled('section')`
+  min-width: 100vw;
+  max-width: 100%;
+  height: fit-content;
+  background-color: blue;
 `
-export const Section = styled('div')`
-    min-width:100vw;
-    max-width:100%;
-    height: fit-content;
-    background-color:black;
-`
+// Section overlap boilerplate
 export const OverlapSection = styled('div')`
-    margin-top:-20%;
-    height: fit-content;
+  margin-top: -20%;
+  height: fit-content;
+`
+// break
+export const SectionBreak = styled('div')`
+  width: 100%;
+  height: 25px;
+  background-color: white;
+`
+
+// Header
+export const HeadSection = styled('div')`
+  top: 0;
+  left: 0;
+  position: fixed;
+  min-width: 100vw;
+  max-width: 100%;
+  height: 80px;
+  box-shadow: 0px 0px 10px 5px #777777;
+  background: transparent;
 `
 export const HeaderOrganiserFlex = styled('div')`
-    display:flex;
-    flex-direction: row;
-    justify-content:space-between;
-    width:100%;
+  width: 100%;
+  height: 100%;
+  padding: 0 30px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
 `
 export const HeaderLogo = styled('img')`
-    height:70px;
-    left:0;
-    padding:5px;
+  height: 70px;
+  left: 0;
 `
 export const HeaderButton = styled('a')`
-    margin-top: auto;
-    margin-bottom:auto;
-    margin-right:15px;
-    height:fit-content;
-    padding:5px;
-    outline: solid black 2px;
-    border-radius:5px;
-    font-size:1.5em;
+  display: block;
+  height: fit-content;
+  padding: 10px 35px;
+  border: 3px solid var(--colorMain);
+  color: var(--colorMain);
+  border-radius: 15px;
+  font-size: 3rem;
+  cursor: pointer;
 `
-    
+
+// Section 1 (HERO)
+export const HeroBG = styled('div')`
+  width: 100%;
+  height: 100vh;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  padding-top: 140px;
+`
+export const HeroOverlay = styled('div')`
+  width: 30%;
+  height: 50%;
+  background-color: orange;
+`
+
+// Section 2 (Overlap)
+export const S2BG = styled('section')`
+  width: 100%;
+  min-height: 50px;
+  display: flex;
+  justify-content: center;
+  margin-top: -275px; // lol idk
+`
+export const S2Vid = styled('div')`
+  width: 60%;
+  height: 500px; //change
+  background-color: pink;
+`
+
+// Section 3 (carousel) THIS IS GOING TO BE A LOT OF FUN!!! HAHAHA
+export const CarouselSec = styled('section')`
+  width: 100%;
+  height: 500px;
+  background-color: purple;
+`
+
+// Section 4 (CTA)
+export const CTABG = styled('section')`
+  width: 100%;
+  height: 600px;
+  background-color: white;
+  display: flex;
+  justify-content: flex-end;
+`
+export const CTARight = styled('div')`
+  width: 30%;
+  height: 100%;
+  background-color: grey;
+  opacity: 0.6;
+`
+
+// Footer
+export const Footer = styled('footer')`
+  width: 100%;
+  height: 300px;
+  background-color: aqua;  
+`
--- mainapp/src/components/GlobalHeader.js
+++ mainapp/src/components/GlobalHeader.js
@@ -1,15 +1,41 @@
-import { HeaderButton, HeaderLogo, HeaderOrganiserFlex, HeadSection } from "../StyledComponents";
+
+import { Section, OverlapSection, SectionBreak, HeadSection,
+  HeaderOrganiserFlex, HeaderLogo, HeaderButton, HeroBG,
+  HeroOverlay, S2BG, S2Vid, CarouselSec, CTABG, CTARight, Footer
+} from "../StyledComponents";
 import logo from "../logo.svg"
-export default function GlobalHeader (){
-    
-    return(
-    <>
+
+export default function GlobalHeader() {
+  return (
+  <>
     <HeadSection>
         <HeaderOrganiserFlex>
-           <HeaderLogo src={logo} alt="poeple" /> 
-           <HeaderButton>Login</HeaderButton>
+            <HeaderLogo src={logo} alt="poeple" /> 
+            <HeaderButton>Login</HeaderButton>
         </HeaderOrganiserFlex>
-    </HeadSection> 
-    </> 
-    )
+    </HeadSection>
+    <HeroBG>
+      <HeroOverlay>
+
+      </HeroOverlay>
+    </HeroBG>
+    <S2BG>
+      <S2Vid>
+
+      </S2Vid>
+    </S2BG>
+    <SectionBreak />
+    <CarouselSec>
+
+    </CarouselSec>
+    <SectionBreak />
+    <CTABG>
+      <CTARight>
+
+      </CTARight>
+    </CTABG>
+    <SectionBreak />
+    <Footer></Footer>
+  </> 
+  )
 }
